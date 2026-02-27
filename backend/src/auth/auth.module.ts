5e2b363344cfe0886589af155e85cf009895b4c0
Shishir Mahato <mahatoshishir6@gmail.com>
2026-02-27 13:07:17 +0530

    Setup JWT strategy and configure Auth module

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from '../prisma/prisma.module';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  providers: [AuthService, AuthResolver]
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      secret: 'SUPER_SECRET_KEY',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
+  providers: [AuthService, AuthResolver, JwtStrategy],
 })
 export class AuthModule {}
---
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,21 @@
+// JWT strategy to validate token
+
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'SUPER_SECRET_KEY', // later move to env
+    });
+  }
+
+  async validate(payload: any) {
+    // Payload will be attached to request.user
+    return payload;
+  }
+}
