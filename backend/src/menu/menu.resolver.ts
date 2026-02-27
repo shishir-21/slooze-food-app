4fcf9332efe4f541155f4b0062a8e0967d07eaed
Shishir Mahato <mahatoshishir6@gmail.com>
2026-02-27 17:17:20 +0530

    Protect getMenu query with JWT guard

--- backend/src/menu/menu.resolver.ts
+++ backend/src/menu/menu.resolver.ts
@@ -1,4 +1,19 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Args } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { MenuService } from './menu.service';
+import { MenuItem } from './entities/menu-item.entity';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
-@Resolver()
-export class MenuResolver {}
+@Resolver(() => MenuItem)
+export class MenuResolver {
+
+  constructor(private menuService: MenuService) {}
+
+  @Query(() => [MenuItem])
+  @UseGuards(JwtAuthGuard)
+  getMenu(
+    @Args('restaurantId') restaurantId: string
+  ) {
+    return this.menuService.getMenuByRestaurant(restaurantId);
+  }
+}
