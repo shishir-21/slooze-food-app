468e01ce6a341c88b86bc6a8a87396986d55c643
Shishir Mahato <mahatoshishir6@gmail.com>
2026-02-28 14:59:58 +0530

    fix(payment): import PrismaModule in PaymentModule to resolve dependency error

--- backend/src/payment/payment.module.ts
+++ backend/src/payment/payment.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentResolver } from './payment.resolver';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [PaymentService, PaymentResolver]
 })
 export class PaymentModule {}
