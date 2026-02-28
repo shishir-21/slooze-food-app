4146ce5321d77777b5358593ce99bbca5e3deb84
Shishir Mahato <mahatoshishir6@gmail.com>
2026-02-28 14:54:47 +0530

    Implement payment service logic

--- backend/src/payment/payment.service.ts
+++ backend/src/payment/payment.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class PaymentService {}
+export class PaymentService {
+
+  constructor(private prisma: PrismaService) {}
+
+  async addPaymentMethod(cardNumber: string, expiry: string, user: any) {
+
+    return this.prisma.paymentMethod.create({
+      data: {
+        cardNumber,
+        expiry,
+        userId: user.userId
+      }
+    });
+  }
+
+  async updatePaymentMethod(id: string, cardNumber: string, expiry: string) {
+    return this.prisma.paymentMethod.update({
+      where: { id },
+      data: { cardNumber, expiry }
+    });
+  }
+}
